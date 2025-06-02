ab87e429ad0fb80c6a3f58fd175012536b768d69
seelamkrishnasri <168399443+seelamkrishnasri@users.noreply.github.com>
2025-06-02 09:58:12 +0530

    Update MultiNumberCalculator.java

--- MultiNumberCalculator.java
+++ MultiNumberCalculator.java
@@ -4,32 +4,22 @@ public class MultiNumberCalculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Ask how many numbers
         System.out.print("Enter the number of values: ");
         int count = scanner.nextInt();
-
-        // Validate input
         if (count < 2) {
             System.out.println("You need at least 2 numbers to perform a calculation.");
             return;
         }
 
         double[] numbers = new double[count];
-
-        // Read numbers
         for (int i = 0; i < count; i++) {
             System.out.print("Enter number " + (i + 1) + ": ");
             numbers[i] = scanner.nextDouble();
         }
-
-        // Choose operation
         System.out.print("Choose operation (+, -, *, /): ");
         char operator = scanner.next().charAt(0);
 
         double result = numbers[0];
-
-        // Perform operation
         for (int i = 1; i < count; i++) {
             switch (operator) {
                 case '+':
